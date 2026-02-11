19b39670c26d201d9a0c0f7a1d86b2b2fbe35bd6
Korneva Anastasia <ivan@mail.ru>
2026-02-11 14:19:41 +0700

    Ввела переменные для задания сторон фигур

--- korneva.py
+++ korneva.py
@@ -2,37 +2,37 @@ from turtle import *
 
 pensize(1)
 
-def big_squareFill(x, y):
+def big_squareFill(x, y, a):
     penup()
     goto(x, y)
     pendown()
     color("orange", "orange")
     begin_fill()
     for i in range(4):
-        forward(100)
+        forward(a)
         left(90)
     end_fill()
     penup()
 
-big_squareFill(0, 0)
+big_squareFill(0, 0, 100)
 
-def big_rectangleFill(x, y):
+def big_rectangleFill(x, y, b, c):
     penup()
     goto(x, y)
     setheading(0)
     pendown()
     color("red", "red")
     begin_fill()
-    forward(100)
+    forward(b)
     left(90)
-    forward(50)
+    forward(c)
     left(90)
-    forward(100)
+    forward(b)
     left(90)
-    forward(50)
+    forward(c)
     end_fill()
     setheading(0)
     penup()
 
-big_rectangleFill(-150, 0)
+big_rectangleFill(-150, 0, 100, 50)
 done()
