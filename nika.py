98e70e56ef1d76db0e0b0aae7437f80723128088
Veronika Timofeeva <veronika.t.2801@mail.ru>
2026-02-12 18:22:43 +0700

    rectangle and isosceles triangle

--- nika.py
+++ nika.py
@@ -1,39 +1,55 @@
 from turtle import *
+import math
 shape("turtle")
 pensize(1)
 pu()
-def triangle_small(x, y):
-    color("blue", "blue")
-    begin_fill()
+def triangle_isosceles(x, y, a, b):
+    """
+    Function, drawing isosceles triangle.
+    :param x: x coordinate in the left corner of the base
+    :param y: y coordinate in the left corner of the base
+    :param a: length of the triangle base
+    :param b: length of the sides of the triangle
+    :return: None
+    """
+
+    angle_base = math.degrees(math.acos(a / (2*b))) #угол при основании
+    angle_top = 180 - 2 * angle_base #угол при вершине
     goto(x, y)
+    color("red", "red")
+    begin_fill()
     pd()
-    fd(50)
-    rt(120)
-    fd(50)
-    rt(120)
-    fd(50)
-    rt(120)
+    fd(a)
+    lt(180 - angle_base)
+    fd(b)
+    lt(180 - angle_top)
+    fd(b)
+    lt(180 - angle_base)
     end_fill()
     pu()
-    rt(60)
-    fd(50)
-    lt(60)
-def square_small(x,y):
-    color("yellow", "yellow")
+triangle_isosceles(100,100,50,100)
+def rectangle(x, y, w, h):
+    """
+    Function, drawing rectangle.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param w: width of a rectangle
+    :param h: height of a rectangle
+    :return: None
+    """
+
+    goto(x,y)
+    color("purple", "purple")
     begin_fill()
-    goto(x, y)
     pd()
-    fd(50)
+    fd(w)
     rt(90)
-    fd(50)
+    fd(h)
     rt(90)
-    fd(50)
+    fd(w)
     rt(90)
-    fd(50)
+    fd(h)
     rt(90)
     end_fill()
     pu()
-    rt(90)
-    fd(50)
-    lt(90)
 done()
