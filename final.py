816ca4aa125606829c0efdf2a18e5c3b766c33ea
Veronika Timofeeva <veronika.t.2801@mail.ru>
2026-02-12 18:59:50 +0700

    isosceles triangle

--- final.py
+++ final.py
@@ -3,3 +3,26 @@ import math
 shape("turtle")
 pensize(1)
 pu()
+def triangle_isosceles(x, y, a, b):
+    """
+    Function, drawing isosceles triangle.
+    :param x: x coordinate in the left corner of the base
+    :param y: y coordinate in the left corner of the base
+    :param a: length of the triangle base
+    :param b: length of the sides of the triangle
+    :return: None
+    """
+    angle_base = math.degrees(math.acos(a / (2*b))) #угол при основании
+    angle_top = 180 - 2 * angle_base #угол при вершине
+    goto(x, y)
+    color("red", "red")
+    begin_fill()
+    pd()
+    fd(a)
+    lt(180 - angle_base)
+    fd(b)
+    lt(180 - angle_top)
+    fd(b)
+    lt(180 - angle_base)
+    end_fill()
+    pu()
